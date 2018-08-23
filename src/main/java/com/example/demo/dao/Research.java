0d88782dc713ff042ca34d31c9510fe14af58b0d
jaeyeon kim <foodsksms@gmail.com>
2018-08-23 11:22:36 +0900

    Research update메소드 수정

--- src/main/java/com/example/demo/dao/Research.java
+++ src/main/java/com/example/demo/dao/Research.java
@@ -28,11 +28,13 @@ public class Research extends CommonSearch {
         getStart(original.getDetailUrl());
         WebDriver driver = getDriver();
         try {
-            String price = driver.findElement(By.xpath("//*[@id=\"topWrap\"]/div[1]/ul[2]/li[1]/em")).getText();
-            String changeMoney = driver.findElement(By.xpath("//*[@id=\"topWrap\"]/div[1]/ul[2]/li[2]/span")).getText();
-            String changePercent = driver.findElement(By.xpath("//*[@id=\"topWrap\"]/div[1]/ul[2]/li[3]")).getText();
-            String profit = driver.findElement(By.xpath("//*[@id=\"performanceCorp\"]/table/tbody/tr[5]/td[9]")).getText();
-            String sales_moeny = driver.findElement(By.xpath("//*[@id=\"performanceCorp\"]/table/tbody/tr[4]/td[9]")).getText();
+            WebElement element = driver.findElement(By.xpath("//*[@id=\"topWrap\"]/div[1]/ul[2]"));
+            String price = element.findElement(By.xpath(".//li[1]/em")).getText();
+            String changeMoney = element.findElement(By.xpath(".//li[2]/span")).getText();
+            String changePercent = element.findElement(By.xpath(".//li[3]")).getText();
+            element = driver.findElement(By.xpath("//*[@id=\"performanceCorp\"]/table/tbody"));
+            String profit = element.findElement(By.xpath(".//tr[5]/td[9]")).getText();
+            String sales_moeny = element.findElement(By.xpath(".//tr[4]/td[9]")).getText();
             String total_cost = driver.findElement(By.xpath("//*[@id=\"stockContent\"]/ul[2]/li[2]/dl[2]/dd")).getText();
             original.update(price, changeMoney, changePercent, profit, sales_moeny, total_cost);
         } catch (org.openqa.selenium.NoSuchElementException e) {

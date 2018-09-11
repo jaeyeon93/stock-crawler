8051209f35d2f0f164c0c183c37ac50ab717d063
jaeyeon kim <foodsksms@gmail.com>
2018-09-11 16:03:10 +0900

    git 수정 후 푸쉬하기

--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -64,9 +64,9 @@ public class StockService {
     @Transactional
     public void addAll() throws Exception {
         long start = System.currentTimeMillis();
-        for (int i = 1; i <= 2;i ++) {
-            stockInfo.partCrawing(i, kospiUrl);
-//            stockInfo.partCrawing(i, kosdaqUrl);
+        for (int i = 1; i <= 4; i++) {
+            stockInfo.bulkInsert(1, kospiUrl);
+            stockInfo.bulkInsert(1, kosdaqUrl);
         }
         long end = System.currentTimeMillis();
         logger.info("총 걸린 시간 : {}초", (end - start)/1000.0);
@@ -108,3 +108,4 @@ public class StockService {
         return stockRepository.findAllByOrderByPriceDesc();
     }
 }
+

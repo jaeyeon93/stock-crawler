4a0c7279dd94360fc72f01aa8a6c6927fcc31649
jaeyeon kim <foodsksms@gmail.com>
2018-10-04 10:40:07 +0900

    webhook 테스트 메소드 구현중

---
+++ src/main/java/com/example/demo/bot/SlackWebhooks.java
@@ -0,0 +1,14 @@
+package com.example.demo.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("slack")
+public class SlackWebhooks {
+    private static final Logger logger =  LoggerFactory.getLogger(SlackWebhooks.class);
+
+
+}

0ef43da208776c8d6c7126256f58e465fe4ee6d9
jaeyeon kim <foodsksms@gmail.com>
2018-10-08 15:21:37 +0900

    Error 수정전

--- src/main/java/com/example/demo/bot/SlackBot.java
+++ src/main/java/com/example/demo/bot/SlackBot.java
@@ -11,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketSession;
@@ -27,6 +30,12 @@ public class SlackBot extends Bot {
 
     @Value("${default.url}")
     private String defaultUrl;
+
+    @Value("${slack.webhook.url}")
+    private String webhookUrl;
+
+    @Value("${slack.bot.url}")
+    private String botUrl;
 
     @Autowired
     private StockService stockService;
@@ -50,8 +59,50 @@ public class SlackBot extends Bot {
     public void onReceiveMessage(WebSocketSession session, Event event, Matcher matcher) throws IOException {
         String message = event.getText();
         RestTemplate restTemplate = new RestTemplate();
-        Stock stock = restTemplate.getForObject(defaultUrl + message, Stock.class);
-        reply(session, event, new Message("응답하라"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        headers.set("Authorization", "Bearer "+ token);
+        String json = "{\n" +
+                "\t\"channel\":\"test\",\n" +
+                "\t\"text\":\"I hope the tour went well, Mr. Wonka.\",\n" +
+                "\t\"attachments\": [\n" +
+                "        {\n" +
+                "            \"color\": \"#CC0000\",\n" +
+                "            \"title\": \"<http://www.naver.com| Naver>\",\n" +
+                "            \"mrkdwn_in\": [\n" +
+                "                \"title\",\n" +
+                "                \"text\",\n" +
+                "                \"pretext\",\n" +
+                "                \"fields\"\n" +
+                "            ],\n" +
+                "            \"fields\": [\n" +
+                "                {\n" +
+                "                    \"title\": \"주가\",\n" +
+                "                    \"value\": \"700,000\",\n" +
+                "                    \"short\": true\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"title\": \"변동률\",\n" +
+                "                    \"value\": \"15%\",\n" +
+                "                    \"short\": true\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"title\": \"변동가격\",\n" +
+                "                    \"value\": \"10,000\",\n" +
+                "                    \"short\": true\n" +
+                "                }\n" +
+                "            ],\n" +
+                "            \"footer\": \"MADE BY JIMMY\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "\n" +
+                "}";
+
+        HttpEntity<String> entity = new HttpEntity<>(json ,headers);
+        logger.info("입력한 메세지 : {}", message);
+        if (message.contains("테스트"))
+            restTemplate.postForEntity(webhookUrl, entity, String.class);
+
         if (message.contains("상위변동률"))
             reply(session, event, new Message("많이 오른 종목 : " + stockService.topRate().toString()));
 
